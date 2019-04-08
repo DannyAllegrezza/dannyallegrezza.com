41442008422bcd31846344a913c46ac2e534b8ba
DannyAllegrezza <danny@dannyallegrezza.com>
2019-04-08 13:18:04 -0400

    lead on what was causing some of my problems
    Needed to change the query itself to fetch ALL posts, and then filter them by their templateKey

--- src/templates/cars-page.js
+++ src/templates/cars-page.js
@@ -8,24 +8,20 @@ import { CarOverview } from '../components/CarOverview';
 
 // The main CarPage Component which is used to render the Cars page
 const CarPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  console.log(data);
+  //const { markdownRemark: post } = data
+  const { edges: cars } = data.allMarkdownRemark;
 
   return (
     <Layout>
-      <CarOverview
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={post.frontmatter.description}
-        helmet={
-          <Helmet
-            titleTemplate="%s | Blog"
-          >
-            <title>{`${post.frontmatter.title}`}</title>
-            <meta name="description" content={`${post.frontmatter.description}`} />
-          </Helmet>
-        }
-        title={post.frontmatter.title}
-      />
+      <section className="section">
+        <div className="container">
+          <div className="content">
+            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+          </div>
+          {cars}
+        </div>
+      </section>
     </Layout>
   )
 }
@@ -39,37 +35,26 @@ CarPage.propTypes = {
 export default CarPage
 
 export const carPageQuery = graphql`
-  query CarPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-        engine
+  query CarPageQuery {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] },
+      filter: { frontmatter: { templateKey: { eq: "cars-page" } }}
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            engine
+            date(formatString: "MMMM DD, YYYY")
+          }
+        }
       }
     }
   }
 `
-
-// export const CarsPagePreview = ({ data }) => {
-//     const PageContent = HTMLContent;
-//     console.log(data);
-//     return (
-//         <Layout>
-//               <section className="section section--gradient">
-//                 <div className="container">
-//                   <div className="columns">
-//                     <div className="column is-10 is-offset-1">
-//                       <div className="section">
-//                         <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-//                           {data.markdownRemark.frontmatter.title}
-//                         </h2>
-//                         <PageContent className="content" content={data.html} />
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </section>
-//           <div>{data.markdownRemark.frontmatter.title}</div>
-//         </Layout>
-//     )
-// }
