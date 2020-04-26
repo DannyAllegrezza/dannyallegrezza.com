e383137bf6d1c19faa3f0cdc64e0cf53d36b8668
Danny Allegrezza <danny@dannyallegrezza.com>
2020-04-25 22:04:45 -0400

    Revert "feat: add prismjs"
    This reverts commit 4cbb404004d5a44f2d83e3c5f3ba2f4afbdc5a96.

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,42 +55,7 @@ module.exports = {
             options: {
               destinationDir: 'static',
             }
-          },
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: "language-",
-              inlineCodeMarker: null,
-              aliases: {},
-              showLineNumbers: false,
-              noInlineHighlight: false,
-              languageExtensions: [
-                {
-                  language: "superscript",
-                  extend: "javascript",
-                  definition: {
-                    superscript_types: /(SuperType)/,
-                  },
-                  insertBefore: {
-                    function: {
-                      superscript_keywords: /(superif|superelse)/,
-                    },
-                  },
-                },
-              ],
-              // Customize the prompt used in shell output
-              // Values below are default
-              prompt: {
-                user: "root",
-                host: "localhost",
-                global: false,
-              },
-              // By default the HTML entities <>&'" are escaped.
-              // Add additional HTML escapes by providing a mapping
-              // of HTML entities and their escape value IE: { '}': '&#123;' }
-              escapeEntities: {},
-            },
-          },
+          }
         ],
       },
     },
